d8c9ccb98ff19139fb6b320095adec4c4992a8df
oswhyteknits <orincywhyte@gmail.com>
2022-08-04 18:02:12 -0400

    added email project to portfolio

--- src/data.js
+++ src/data.js
@@ -1,46 +1,56 @@
 export default {
-    gridData: {
-      grid: [
-        {
-          id: "1",
-          img: "images/image1.png",
-          header: "Rick and Morty",
-          text: "Calling the Rick and Morty API to creating a social media experience for the characters of the hit show.",
-          color: "#66da42",
-          live: "https://rickandmortyapi-react.netlify.app/",
-          code: "https://github.com/oswhytecodes/rickandmorty-react",
-          alt: "Rick and Morty homepage"
-        },
-        {
-          id: "2",
-          img: "images/image2.png",
-          header: "Reddit-lite",
-          text: "Redesigning a light version of the Reddit app.",
-          color: "#66da42",
-          live: "https://oswhyte-redditlite.netlify.app/",
-          code: "https://github.com/oswhytecodes/reddit-lite",
-          alt: "Redditlite"
-        },
-        {
-          id: "3",
-          img: "images/image3.png",
-          header: "Frontend Dev",
-          text: "Using Tailwind to build a beautiful design experience, showcasing my designs and coding skills.",
-          color: "#66da42",
-          live: "https://oswhytecodes.github.io/orincywhyte_frontendengineer/",
-          code: "https://github.com/oswhytecodes/orincywhyte_frontendengineer",
-          alt: "Orincy Whyte Developer portfolio"
-        },
-        {
-          id: "4",
-          img: "images/image4.png",
-          header: "Another Todo",
-          text: "The perfect project to exploring filtering and deleting data.",
-          color: "#66da42",
-          live: "https://oswhyte-todoapp-react.netlify.app/",
-          code: "https://github.com/oswhytecodes/TodoApp-React",
-          alt: "Todo App in React"
-        },
-      ],
-    }
-}
+  gridData: {
+    grid: [
+      {
+        id: "1",
+        img: "images/image1.png",
+        header: "Rick and Morty",
+        text: "Calling the Rick and Morty API to creating a social media experience for the characters of the hit show.",
+        color: "#66da42",
+        live: "https://rickandmortyapi-react.netlify.app/",
+        code: "https://github.com/oswhytecodes/rickandmorty-react",
+        alt: "Rick and Morty homepage",
+      },
+      {
+        id: "5",
+        img: "images/emails.png",
+        header: "Email Campaigns",
+        text: "Using HTML tables to build a variety of email tamplates.",
+        color: "#66da42",
+        live: "https://oswhytecodes.github.io/emailcampaign/index.html",
+        code: "https://github.com/oswhytecodes/emailcampaign",
+        alt: "Email Templates with HTML tables",
+      },
+      {
+        id: "2",
+        img: "images/image2.png",
+        header: "Reddit-lite",
+        text: "Redesigning a light version of the Reddit app.",
+        color: "#66da42",
+        live: "https://oswhyte-redditlite.netlify.app/",
+        code: "https://github.com/oswhytecodes/reddit-lite",
+        alt: "Redditlite",
+      },
+      {
+        id: "3",
+        img: "images/image3.png",
+        header: "Frontend Dev",
+        text: "Using Tailwind to build a beautiful design experience, showcasing my designs and coding skills.",
+        color: "#66da42",
+        live: "https://oswhytecodes.github.io/orincywhyte_frontendengineer/",
+        code: "https://github.com/oswhytecodes/orincywhyte_frontendengineer",
+        alt: "Orincy Whyte Developer portfolio",
+      },
+      {
+        id: "4",
+        img: "images/image4.png",
+        header: "Another Todo",
+        text: "The perfect project to exploring filtering and deleting data.",
+        color: "#66da42",
+        live: "https://oswhyte-todoapp-react.netlify.app/",
+        code: "https://github.com/oswhytecodes/TodoApp-React",
+        alt: "Todo App in React",
+      },
+    ],
+  },
+};
